f4e803d9d5228b137f9dbc507e926081608ca4a2
Adam Sisk <adamsisk91@gmail.com>
2019-11-05 10:54:40 -0600

    adds validations to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
+  validates :body, presence: true
+  validates :votes, presence: true
+  validates :votes, numericality: { only_integer: true }
   belongs_to :post
 end
