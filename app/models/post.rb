5ea303297cf2f3b122db14359b36053d395da705
Adam Sisk <adamsisk91@gmail.com>
2019-11-05 11:36:11 -0600

    adds validations to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :votes, presence: true
+  validates :votes, numericality: { only_integer: true }
+  has_many :comments
 end
