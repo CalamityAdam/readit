f2c5fade39c44d90bba2e971b0aaec3879437be9
Adam Sisk <adamsisk91@gmail.com>
2019-11-11 16:47:38 -0600

    accidentally removed a couple routes, they're back

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create] do
-
+    post '/upvote', to: 'posts#upvote'
+    post '/downvote', to: 'posts#downvote'
     resources :comments, only: [:create,] do
+      post '/upvote', to: 'comments#upvote'
+      post '/downvote', to: 'comments#downvote'
     end
   end
 end
